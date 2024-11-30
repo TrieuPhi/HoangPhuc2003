880237beb40d97fa1c8245c9867d3661ffe73450
Dai Trieu Phi <dtptrieuphidtp@gmail.com>
2024-11-30 14:17:58 +0700

    3rd commit

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function firstQuestion(){
     Swal.fire({
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
-        imageUrl: 'img/HoangPhuc.jpg',
+        imageUrl: 'img/HoangPhuc.png',
         imageWidth: 300,
         imageHeight: 300,
         background: '#fff url("img/iput-bg.jpg")',
